4e81abcb3a382b8ebc51a088bc4b2437b143f82b
Sunnie <sunniejpeng@gmail.com>
2017-06-11 09:36:01 +0800

    HullWhiteTree

---
+++ hw11_HullWhiteTree/main.cpp
@@ -0,0 +1,16 @@
+//  main.cpp
+//  hw11_HullWhiteTree
+
+
+#include <iostream>
+#include "HullWhiteTree.h"
+
+int main()
+{
+    HullWhiteTree t1;
+    
+    t1.get_gamma();
+    t1.get_rt();
+    t1.print_gamma();
+    t1.print_tree();
+}
